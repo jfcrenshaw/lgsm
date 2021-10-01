Fixed some jax tracer leakage issues, simplified some of the modules, and simplified the config structure.
Former-commit-id: d1fee3e7b1c7b1c3c8bf865b24fc637803ff5931 [formerly c88f50671660d3ade0ed663e829368907cd5f29d]

Former-commit-id: 86e07e385fd77bd68362a458ae95d6f147ab1e62

lgsm/lgsmodel.py
@@ -2,10 +2,11 @@
 from typing import Sequence
 
 import elegy
+import jax.numpy as jnp
 import numpy as np
 
-from .physics_layer import PhysicsLayer
-from .vae import VAE
+from lgsm.physics_layer import PhysicsLayer
+from lgsm.vae import VAE
 
 
 class StandardScaler(elegy.Module):
@@ -33,9 +34,9 @@ class LGSModel(elegy.Module):
         intrinsic_latent_size: int,
         # Decoder settings
         decoder_layers: Sequence[int],
-        sed_min: float,
-        sed_max: float,
-        sed_bins: int,
+        wave_min: float,
+        wave_max: float,
+        wave_bins: int,
         normalize_at: float,
         sed_unit: str,
         # VAE settings
@@ -45,19 +46,42 @@ class LGSModel(elegy.Module):
         band_oversampling: int,
     ):
         super().__init__()
+
+        # save the config
         self.input_mean = input_mean
         self.input_std = input_std
         self.encoder_layers = encoder_layers
         self.intrinsic_latent_size = intrinsic_latent_size
         self.decoder_layers = decoder_layers
-        self.sed_min = sed_min
-        self.sed_max = sed_max
-        self.sed_bins = sed_bins
         self.normalize_at = normalize_at
         self.sed_unit = sed_unit
         self.batch_norm = batch_norm
         self.bandpasses = bandpasses
         self.band_oversampling = band_oversampling
+
+        # setup SED wavelength grid
+        dwave = (wave_max - wave_min) / (wave_bins - 1)
+        sed_wave = jnp.arange(wave_min, wave_max + dwave, dwave)
+        self.sed_wave = sed_wave
+
+        # instantiate the VAE
+        self.vae = VAE(
+            encoder_layers=encoder_layers,
+            intrinsic_latent_size=intrinsic_latent_size,
+            decoder_layers=decoder_layers,
+            sed_wave=sed_wave,
+            normalize_at=normalize_at,
+            sed_unit=sed_unit,
+            batch_norm=batch_norm,
+        )
+
+        # instantiate the Physics Layer
+        self.physics_layer = PhysicsLayer(
+            sed_wave=sed_wave,
+            sed_unit=self.sed_unit,
+            bandpasses=self.bandpasses,
+            band_oversampling=self.band_oversampling,
+        )
 
     def call(self, inputs: np.ndarray) -> dict:
 
@@ -68,27 +92,10 @@ class LGSModel(elegy.Module):
         inputs = StandardScaler(self.input_mean, self.input_std)(inputs)
 
         # pass the scaled inputs through the VAE
-        vae_outputs = VAE(
-            encoder_layers=self.encoder_layers,
-            intrinsic_latent_size=self.intrinsic_latent_size,
-            decoder_layers=self.decoder_layers,
-            wave_min=self.wave_min,
-            wave_max=self.wave_max,
-            wave_bins=self.wave_bins,
-            normalize_at=self.normalize_at,
-            sed_unit=self.sed_unit,
-            batch_norm=self.batch_norm,
-        )(inputs)
+        vae_outputs = self.vae(inputs)
 
         # predict photometry for the intrinsic SEDs
-        predicted_photometry = PhysicsLayer(
-            self.wave_min,
-            self.wave_max,
-            self.wave_bins,
-            self.sed_unit,
-            self.bandpasses,
-            self.band_oversampling,
-        )(
+        predicted_photometry = self.physics_layer(
             sed=vae_outputs[f"sed_{self.sed_unit}"],
             amplitude=vae_outputs["amplitude"],
             redshift=redshift,

lgsm/physics_layer.py
@@ -10,7 +10,7 @@ import sncosmo
 from jax import vmap
 from sncosmo.constants import HC_ERG_AA
 
-from .sed_utils import mag_to_flambda
+from lgsm.sed_utils import mag_to_flambda
 
 
 class PhysicsLayer(elegy.Module):
@@ -34,37 +34,35 @@ class PhysicsLayer(elegy.Module):
 
     def __init__(
         self,
-        wave_min: float,
-        wave_max: float,
-        wave_bins: int,
+        sed_wave: np.ndarray,
         sed_unit: str,
         bandpasses: Sequence[str],
         band_oversampling: int,
     ):
         super().__init__()
-        self.wave_min = wave_min
-        self.wave_max = wave_max
-        self.wave_bins = wave_bins
+
+        # make sure that sed_wave has even sampling
+        dlambda = np.diff(sed_wave)
+        assert np.all(dlambda == dlambda[0]), "sed_wave must be an evenly-spaced grid."
+
+        # save config
+        self.sed_wave = sed_wave
         self.sed_unit = sed_unit
         self.bandpasses = bandpasses
         self.band_oversampling = band_oversampling
 
-        # save the SED wavelength grid
-        self.dwave = (wave_max - wave_min) / (wave_bins - 1)
-        self.wave = np.arange(self.wave_min, self.wave_max + self.dwave, self.dwave)
-
         # setup functions to handle sed units
         if self.sed_unit == "mag":
             self.scale_sed = lambda sed, amplitude: amplitude + sed
-            self.convert_sed_units = lambda sed: mag_to_flambda(sed, self.wave)
+            self.convert_sed_units = lambda sed: mag_to_flambda(sed, self.sed_wave)
         else:  # sed_unit == "flambda"
             self.scale_sed = lambda sed, amplitude: amplitude * sed
             self.convert_sed_units = lambda sed: sed
 
         # precompute the  weights for flux integration
-        self._setup_band_weights()
+        self.band_wave, self.band_weights = self._get_band_weights()
 
-    def _setup_band_weights(self):
+    def _get_band_weights(self):
         """Precompute the weights for flux integration so they can be reused
         over and over again!
         By our definition,
@@ -82,10 +80,11 @@ class PhysicsLayer(elegy.Module):
 
         # wavelength grid for bandpass is essentially the same as wave,
         # potentially with oversampling
-        band_dwave = self.dwave / self.band_oversampling
+        dwave = self.sed_wave[1] - self.sed_wave[0]
+        band_dwave = dwave / self.band_oversampling
         band_wave = jnp.arange(
-            self.wave_min - band_dwave * pad,
-            self.wave_max + self.dwave + band_dwave * pad,
+            self.sed_wave.min() - band_dwave * pad,
+            self.sed_wave.max() + dwave + band_dwave * pad,
             band_dwave,
         )
 
@@ -116,9 +115,10 @@ class PhysicsLayer(elegy.Module):
             )
             band_weights.append(conv_weights)
 
-        # save the band wavelength grid and the convolved weights
-        self.band_wave = jnp.array(band_wave[pad : -pad or None])
-        self.band_weights = jnp.array(band_weights)
+        # return the band wavelength grid and the convolved weights
+        band_wave = band_wave[pad : -pad or None]
+        band_weights = jnp.array(band_weights)
+        return band_wave, band_weights
 
     def _calc_mag_single_sed(self, sed_flambda, redshift, band_weights):
 
@@ -127,7 +127,7 @@ class PhysicsLayer(elegy.Module):
         # far more accurate than redshifting SED - at least when comparing
         # to calculations with sncosmo...)
         band_weights = jnp.interp(
-            self.wave,
+            self.sed_wave,
             self.band_wave / (1 + redshift),
             band_weights,
             left=0,

lgsm/vae.py
@@ -90,9 +90,7 @@ class Decoder(elegy.Module):
     def __init__(
         self,
         layers: Sequence[int],
-        wave_min: float,
-        wave_max: float,
-        wave_bins: int,
+        sed_wave: np.ndarray,
         normalize_at: float,
         sed_unit: str,
         batch_norm: bool,
@@ -100,12 +98,9 @@ class Decoder(elegy.Module):
         super().__init__()
 
         self.layers = layers
+        self.sed_wave = sed_wave
         self.normalize_at = normalize_at
         self.sed_unit = sed_unit
-
-        # setup the wavelength array
-        dwave = (wave_max - wave_min) / (wave_bins - 1)
-        self.sed_wave = jnp.arange(wave_min, wave_max + dwave, dwave)
 
         # define the function to normalize the SEDs
         if sed_unit == "mag":
@@ -123,7 +118,7 @@ class Decoder(elegy.Module):
     def call(self, intrinsic_latents: np.ndarray) -> dict:
 
         # construct the first decoder layers
-        for layer in self.decoder_layers:
+        for layer in self.layers:
             intrinsic_latents = elegy.nn.Linear(layer)(intrinsic_latents)
             intrinsic_latents = self.NormLayer()(intrinsic_latents)
             intrinsic_latents = jax.nn.relu(intrinsic_latents)
@@ -138,7 +133,8 @@ class Decoder(elegy.Module):
         ).reshape(-1, 1)
         sed = self.normalize(sed, norm)
 
-        return {f"sed_{self.sed_unit}": sed}
+        # return the wavelength grid and the normalized sed!
+        return {"sed_wave": self.sed_wave, f"sed_{self.sed_unit}": sed}
 
 
 class VAE(elegy.Module):
@@ -151,9 +147,7 @@ class VAE(elegy.Module):
         intrinsic_latent_size: int,
         # Decoder settings
         decoder_layers: Sequence[int],
-        wave_min: float,
-        wave_max: float,
-        wave_bins: int,
+        sed_wave: np.ndarray,
         normalize_at: float,
         sed_unit: str,
         # global settings
@@ -161,34 +155,37 @@ class VAE(elegy.Module):
     ):
         super().__init__()
 
+        # save the config
         self.encoder_layers = encoder_layers
         self.intrinsic_latent_size = intrinsic_latent_size
         self.decoder_layers = decoder_layers
-        self.wave_min = wave_min
-        self.wave_max = wave_max
-        self.wave_bins = wave_bins
+        self.sed_wave = sed_wave
         self.normalize_at = normalize_at
         self.sed_unit = sed_unit
         self.batch_norm = batch_norm
 
+        # instantiate the encoder
+        self.encoder = Encoder(
+            layers=encoder_layers,
+            intrinsic_latent_size=intrinsic_latent_size,
+            batch_norm=batch_norm,
+        )
+
+        # instantiate the decoder
+        self.decoder = Decoder(
+            layers=decoder_layers,
+            sed_wave=sed_wave,
+            normalize_at=normalize_at,
+            sed_unit=sed_unit,
+            batch_norm=batch_norm,
+        )
+
     def call(self, inputs: np.ndarray) -> dict:
 
         # encode the photometry in the implicit/explicit latent space
-        latents = Encoder(
-            layers=self.encoder_layers,
-            intrinsic_latent_size=self.intrinsic_latent_size,
-            batch_norm=self.batch_norm,
-        )(inputs)
+        latents = self.encoder(inputs)
 
         # decode into an SED
-        sed = Decoder(
-            layers=self.decoder_layers,
-            wave_min=self.wave_min,
-            wave_max=self.wave_max,
-            wave_bins=self.wave_bins,
-            normalize_at=self.normalize_at,
-            sed_unit=self.sed_unit,
-            batch_norm=self.batch_norm,
-        )(latents["intrinsic_latents"])
+        sed = self.decoder(latents["intrinsic_latents"])
 
         return {**latents, **sed}